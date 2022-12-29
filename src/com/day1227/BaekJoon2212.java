278947451c9dbdac50c9ae72476c985f9e69fd09
이현진 <dnzma13@naver.com>
2022-12-29 17:57:16 +0900

    initial commit

---
+++ src/com/day1227/BaekJoon2212.java
@@ -0,0 +1,42 @@
+package com.day1227;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+// 센서
+public class BaekJoon2212 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] sensors = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            sensors[i] = Integer.parseInt(st.nextToken());
+        }
+        // 정렬
+        Arrays.sort(sensors);
+        // 센서들 사이의 거리 차이를 담을 배열
+        Integer[] diff = new Integer[N - 1];
+        for (int i = 0; i < N - 1; i++) {
+            diff[i] = sensors[i + 1] - sensors[i];
+        }
+        // 센서들 사이 거리 내림차순 정렬
+        Arrays.sort(diff, Collections.reverseOrder());
+
+        // diff 배열 k-1 ~ 마지막까지 합
+        // 첫번째
+        int sum = 0;
+        for (int i = K - 1; i < N - 1; i++) {
+            sum += diff[i];
+        }
+        System.out.println(sum);
+    }
+}

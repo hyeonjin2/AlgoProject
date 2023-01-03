044cd8b4ec339a955731d27f86e9c03a11aeb032
이현진 <dnzma13@naver.com>
2023-01-04 00:29:40 +0900

    soving BaekJoon1516

--- src/com/day0103/BaekJoon1516.java
+++ src/com/day0103/BaekJoon1516.java
@@ -4,24 +4,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 // 게임 개발
 public class BaekJoon1516 {
     static class Node {
-        int to, weigth;
-        Node from;
+        int vertex;
+        Node to;
 
-        public Node(int to, int weigth, Node from) {
+        public Node(int from, Node to) {
+            this.vertex = from;
             this.to = to;
-            this.weigth = weigth;
-            this.from = from;
         }
     }
 
-    static int N, ans;
-    static int[] order;
+    static int N;
+    static int[] inDegree, build, times;
     static Node[] adjList;
 
     public static void main(String[] args) throws IOException {
@@ -29,29 +29,57 @@ public class BaekJoon1516 {
 
         N = Integer.parseInt(br.readLine());
 
-        order = new int[N];
-        adjList = new Node[N];
+        inDegree = new int[N + 1];
+        times = new int[N + 1];
+        build = new int[N + 1];
+        adjList = new Node[N + 1];
 
         StringTokenizer st;
-        for (int from = 0; from < N; from++) {
+        for (int from = 1; from <= N; from++) {
             st = new StringTokenizer(br.readLine());
-            int weight = Integer.parseInt(st.nextToken());
+            int time = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
-            adjList[from] = new Node(to, weight, adjList[from]);
-            order[from]++;
+            times[from] = time;
+            while (to != -1) {
+                adjList[to] = new Node(from, adjList[to]);
+                inDegree[from]++;
+                to = Integer.parseInt(st.nextToken());
+            }
         }
-        ans = 0;
+//        System.out.println(Arrays.toString(times));
+//        System.out.println(Arrays.toString(inDegree));
         TopologySort();
-
-        System.out.println(ans);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            sb.append(build[i]).append("\n");
+        }
+        System.out.println(sb);
     }
 
     private static void TopologySort() {
         Queue<Integer> queue = new ArrayDeque<>();
-        for (int i = 0; i < N; i++) {
-            if (order[i] == 0) {
-                ans += adjList[i].weigth;
+        for (int i = 1; i <= N; i++) {
+            if (inDegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            build[cur] += times[cur];
+
+            for (Node temp = adjList[cur]; temp != null; temp = temp.to) {
+                if (--inDegree[temp.vertex] == 0) {
+                    build[temp.vertex] += build[cur];
+                    queue.offer(temp.vertex);
+                }
             }
         }
     }
 }
+/*
+4
+20 -1
+10 -1
+1 1 2 -1
+1000 1 2 3 -1
+ */

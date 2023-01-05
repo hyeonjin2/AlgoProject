fa8e7f2f0b3bec9b6cf04e8fcb13e22ea662b786
dnzma13 <dnzma13@naver.com>
2023-01-05 17:55:22 +0900

    solving BaekJoon1655

--- src/com/day0104/BaekJoon1655.java
+++ src/com/day0104/BaekJoon1655.java
@@ -6,23 +6,25 @@ import java.io.InputStreamReader;
 
 // 가운데를 말해요
 public class BaekJoon1655 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int ans = Integer.parseInt(br.readLine());
+        int[] nums = new int[N];
 
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(br.readLine());
+        }
+        int[] parents = new int[N];
+        // subset
+        for (int i = 0; i < N; i++) {
+            parents[i] = nums[i];
+        }
+
+        int ind = 0;
         for (int i = 1; i < N; i++) {
-            int temp = Integer.parseInt(br.readLine());
-            if (ans > temp) {
-                ans = temp;
-            }
-            // ans <= temp
-            else {
-                if (i % 2 == 1) {
-//                    ans = ;
-                } else {
-
-                }
+            if (nums[ind] > nums[i]) {
+//                parents[ind]
             }
         }
     }
